0916f4c4fc2e128c8fdf028064ad60ae77f3616d
kdhttps <kdhttps@gmail.com>
2021-09-20 15:49:34 +0530

    refactor(login-router.spec.js): apply dry

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,6 +1,6 @@
 class LoginRouter {
   route (httpRequest) {
-    const { email, password } = httpRequest
+    const { email, password } = httpRequest.body
     if (!email || !password) {
       return {
         statusCode: 400
