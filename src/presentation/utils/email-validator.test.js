a081fc44571ce321fd2556304feac251f84cc847
kdhttps <kdhttps@gmail.com>
2021-09-23 18:35:02 +0530

    test(mock): mock validator

--- src/presentation/utils/email-validator.test.js
+++ src/presentation/utils/email-validator.test.js
@@ -1,13 +1,22 @@
+const validator = require('validator')
+
 class EmailValidator {
   isValid (email) {
-    return true
+    return validator.isEmail(email)
   }
 }
 
 describe('Email validator', () => {
   test('Should return true if validator returns true', () => {
     const sut = new EmailValidator()
-    const isEmailValid = sut.isValid('loki@gmail.com')
+    const isEmailValid = sut.isValid('captain@gmail.com')
     expect(isEmailValid).toBe(true)
+  })
+
+  test('Should return false if validator returns false', () => {
+    const sut = new EmailValidator()
+    validator.isEmailValid = false
+    const isEmailValid = sut.isValid('loki@gmail.com')
+    expect(isEmailValid).toBe(false)
   })
 })
