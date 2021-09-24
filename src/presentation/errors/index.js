ed4ac2822cf291de09bc1c4136893a76f0f482bd
kdhttps <kdhttps@gmail.com>
2021-09-24 13:49:54 +0530

    refactor(errors): move generic errors to util layer

--- src/presentation/errors/index.js
+++ src/presentation/errors/index.js
@@ -1,11 +1,7 @@
-const MissingParamError = require('./missing-param.error')
 const ServerError = require('./server.error')
 const UnauthenticatedError = require('./unauthenticated.error')
-const InvalidParamError = require('./invalid-param.error')
 
 module.exports = {
-  MissingParamError,
   ServerError,
-  UnauthenticatedError,
-  InvalidParamError
+  UnauthenticatedError
 }
