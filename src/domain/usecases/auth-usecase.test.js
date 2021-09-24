63835a04993746116dbecc2d6a650d4ba3a798b6
kdhttps <kdhttps@gmail.com>
2021-09-24 15:44:39 +0530

    test(auth-usecase.test.js): should call LoadUserByEmailResository with correct email

--- src/domain/usecases/auth-usecase.test.js
+++ src/domain/usecases/auth-usecase.test.js
@@ -1,5 +1,9 @@
 const { MissingParamError } = require('../../utils/errors')
 class AuthUseCase {
+  constructor (loadUserByEmailResository) {
+    this.loadUserByEmailResository = loadUserByEmailResository
+  }
+
   async auth (email, password) {
     if (!email) {
       throw new MissingParamError('email')
@@ -7,6 +11,7 @@ class AuthUseCase {
     if (!password) {
       throw new MissingParamError('password')
     }
+    await this.loadUserByEmailResository.load(email, password)
   }
 }
 
@@ -21,5 +26,19 @@ describe('Auth UseCase', () => {
     const sut = new AuthUseCase()
     const promise = sut.auth('captain@gmail.com')
     expect(promise).rejects.toThrow(new MissingParamError('password'))
+  })
+
+  test('should call LoadUserByEmailResository with correct email', async () => {
+    class LoadUserByEmailResositorySpy {
+      async load (email) {
+        this.email = email
+      }
+    }
+
+    const loadUserByEmailResository = new LoadUserByEmailResositorySpy()
+    const sut = new AuthUseCase(loadUserByEmailResository)
+    const email = 'captain@gmail.com'
+    await sut.auth(email, 'abc123')
+    expect(loadUserByEmailResository.email).toBe(email)
   })
 })
