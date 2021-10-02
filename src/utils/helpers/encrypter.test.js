f794201af7f6d0cfe3d5c78edac0a1bab3106f29
kdhttps <kdhttps@gmail.com>
2021-10-02 18:40:15 +0530

    test(encrypter): put sut in seperate factory function

--- src/utils/helpers/encrypter.test.js
+++ src/utils/helpers/encrypter.test.js
@@ -7,22 +7,27 @@ class Encrypter {
   }
 }
 
+const makeSut = () => {
+  const sut = new Encrypter()
+  return sut
+}
+
 describe('Encrypter', () => {
   test('should return true in bcrypt returns true', async () => {
-    const sut = new Encrypter()
+    const sut = makeSut()
     const isValid = await sut.compare('any_value', 'hashed_value')
     expect(isValid).toBe(true)
   })
 
   test('should return false in bcrypt returns false', async () => {
-    const sut = new Encrypter()
+    const sut = makeSut()
     bcrypt.isValid = false
     const isValid = await sut.compare('any_value', 'hashed_value')
     expect(isValid).toBe(false)
   })
 
   test('should call bcrypt with correct values', async () => {
-    const sut = new Encrypter()
+    const sut = makeSut()
     const value = 'any_value'
     const hash = 'any_hash'
     await sut.compare(value, hash)
