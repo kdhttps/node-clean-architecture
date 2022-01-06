c207619292843999e61297be0068513f58daa9b7
kdhttps <kdhttps@gmail.com>
2022-01-06 14:39:55 +0530

    test(loaduserbyemail): add jest mongodb setup and test

--- jest.config.js
+++ jest.config.js
@@ -7,5 +7,6 @@ module.exports = {
   clearMocks: true,
   collectCoverage: true,
   coverageDirectory: 'coverage',
-  testEnvironment: 'node'
+  // testEnvironment: 'node'
+  preset: '@shelf/jest-mongodb'
 }
--- src/infra/repositories/load-user-by-email-repository.test.js
+++ src/infra/repositories/load-user-by-email-repository.test.js
@@ -1,12 +1,50 @@
+const { MongoClient } = require('mongodb')
+
 class LoadUserByEmailRepository {
-  load (email) {
-    return null
+  constructor (userModel) {
+    this.userModel = userModel
+  }
+
+  async load (email) {
+    const user = await this.userModel.findOne({ email })
+    return user
   }
 }
 describe('LoadUserByEmail repository', () => {
-  test('should return null if no user is found', () => {
-    const sut = new LoadUserByEmailRepository()
-    const user = sut.load('invalid@gmail.com')
+  let connection
+  let db
+
+  beforeAll(async () => {
+    connection = await MongoClient.connect(global.__MONGO_URI__, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+    db = await connection.db()
+  })
+
+  beforeEach(async () => {
+    await db.collection('users').deleteMany()
+  })
+
+  afterAll(async () => {
+    await connection.close()
+  })
+
+  test('should return null if no user is found', async () => {
+    const userModel = db.collection('users')
+    const sut = new LoadUserByEmailRepository(userModel)
+    const user = await sut.load('invalid@gmail.com')
     expect(user).toBeNull()
+  })
+
+  test('should return an user if user is found', async () => {
+    const validUserEmail = 'valid@gmail.com'
+    const userModel = db.collection('users')
+    const mockUser = { _id: 'some-user-id', email: validUserEmail }
+    await userModel.insertOne(mockUser)
+
+    const sut = new LoadUserByEmailRepository(userModel)
+    const user = await sut.load(validUserEmail)
+    expect(user.email).toBe(validUserEmail)
   })
 })
