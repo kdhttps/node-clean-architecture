edbc965c5f4c3c1c03171b94e5ee91dcd3962200
kdhttps <kdhttps@gmail.com>
2022-01-06 16:09:08 +0530

    test(load-user-by-email-repo): should throw error if no email is provided

--- src/infra/repositories/load-user-by-email-repository.test.js
+++ src/infra/repositories/load-user-by-email-repository.test.js
@@ -1,5 +1,6 @@
 const MongoHelper = require('../helpers/mongo-helper')
 const LoadUserByEmailRepository = require('./load-user-by-email-repository')
+const MissingParamError = require('../../utils/errors/missing-param.error')
 
 let db
 
@@ -46,5 +47,11 @@ describe('LoadUserByEmail repository', () => {
     const sut = new LoadUserByEmailRepository()
     const promise = sut.load('test@test.com')
     expect(promise).rejects.toThrow()
+  })
+
+  test('should throw error if no email is provided', async () => {
+    const { sut } = makeSut()
+    const promise = sut.load()
+    expect(promise).rejects.toThrow(new MissingParamError('email'))
   })
 })
