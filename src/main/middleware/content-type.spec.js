eac2b470f52d2af7988acf4e396c916710f16531
kdhttps <kdhttps@gmail.com>
2022-01-06 23:52:15 +0530

    test(setup.spec.js): should return content-type xml if forced

--- src/main/middleware/content-type.spec.js
+++ src/main/middleware/content-type.spec.js
@@ -4,11 +4,22 @@ const app = require('../config/app')
 describe('App Setup', () => {
   test('should return content-type json as a default', async () => {
     app.get('/test_content_type', (req, res) => {
-      res.send({})
+      res.send('')
     })
 
     await request(app)
       .get('/test_content_type')
       .expect('content-type', /json/)
+  })
+
+  test('should return content-type xml if forced', async () => {
+    app.get('/test_content_type_xml', (req, res) => {
+      res.type('xml')
+      res.send('')
+    })
+
+    await request(app)
+      .get('/test_content_type_xml')
+      .expect('content-type', /xml/)
   })
 })
