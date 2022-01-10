4e4d117281f5d515559e65baefe5d0d8f0b0f343
kdhttps <kdhttps@gmail.com>
2022-01-10 18:43:55 +0530

    test(index.js): test app listen

--- src/main/index.js
+++ src/main/index.js
@@ -1,9 +1,5 @@
 const app = require('./config/app')
 
-app.get('/mango', (req, res) => {
-  res.send('manog')
-})
-
 app.listen(8585, () => {
   console.log('running...')
 })
---
+++ src/main/index.test.js
@@ -0,0 +1,15 @@
+describe('Index', () => {
+  test('should call app listen', () => {
+    jest.mock('./config/app', () => ({
+      listen (port, callback) {
+        if (callback) {
+          callback()
+        }
+      }
+    }))
+    const mock = jest.requireMock('./config/app')
+    const listen = jest.spyOn(mock, 'listen')
+    require('./index')
+    expect(listen).toHaveBeenCalledTimes(1)
+  })
+})
