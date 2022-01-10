f96611d8c0bad010e90743b7ed09de873fd44060
kdhttps <kdhttps@gmail.com>
2022-01-10 18:06:58 +0530

    refactor(mongo-healper): make sut sepearate factory function

--- src/infra/helpers/mongo-helper.test.js
+++ src/infra/helpers/mongo-helper.test.js
@@ -1,17 +1,21 @@
 const MongoHelper = require('./mongo-helper')
 
+const makeSut = async () => {
+  const sut = MongoHelper
+  await sut.connect(global.__MONGO_URI__)
+  return sut
+}
+
 describe('Mongo Helper', () => {
   test('should set db and connection after connection', async () => {
-    const sut = MongoHelper
-    await sut.connect(global.__MONGO_URI__)
+    const sut = await makeSut()
     expect(MongoHelper.db).toBeTruthy()
     expect(MongoHelper.connection).toBeTruthy()
     sut.close()
   })
 
   test('should unset db and connection after close', async () => {
-    const sut = MongoHelper
-    await sut.connect(global.__MONGO_URI__)
+    const sut = await makeSut()
     await sut.close()
     expect(MongoHelper.db).toBeFalsy()
     expect(MongoHelper.connection).toBeFalsy()
