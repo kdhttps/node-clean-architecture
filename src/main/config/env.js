a6cd670848fa399a67dfe035c5a11fd3b910ad9e
kdhttps <kdhttps@gmail.com>
2022-01-11 18:44:22 +0530

    refactor(login-router): move composer to its own file

--- src/main/config/env.js
+++ src/main/config/env.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-code'
+  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-code',
+  tokenSecret: process.env.TOKEN_SECRET || 'secretabc'
 }
