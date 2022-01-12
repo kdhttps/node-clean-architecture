51e57102c1e5e3809fbd6afae037bdc10acc6b3a
kdhttps <kdhttps@gmail.com>
2022-01-12 13:59:16 +0530

    feat(index.js): add port

--- src/main/index.js
+++ src/main/index.js
@@ -5,7 +5,7 @@ mongoHelper.connect(env.mongoUrl)
   .then(() => {
     const app = require('./config/app')
     app.listen(env.port, () => {
-      console.log('running...')
+      console.log(`Server is running at localhost:${env.port}`)
     })
   })
   .catch(console.error)
